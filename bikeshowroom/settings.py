ce1ed39835ddd6d34307b9228ba42c4013963b72
reshetha08 <reshesiva444@gmail.com>
2026-07-18 18:06:04 +0530

    Add Railway domain to ALLOWED_HOSTS

--- bikeshowroom/settings.py
+++ bikeshowroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-11289.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
