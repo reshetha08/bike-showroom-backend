0df82da389576220abf1bcec9b1671b30f2f928c
reshetha08 <reshesiva444@gmail.com>
2026-07-19 11:53:55 +0530

    Fix storage backend for Django 5+ compatibility

--- bikeshowroom/settings.py
+++ bikeshowroom/settings.py
@@ -142,7 +142,14 @@ CLOUDINARY_STORAGE = {
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
